4d7203b646000f709fd6c7dc896ee6bef6d2af18
nuh.yurduseven <nuh.yurduseven@turksat.com.tr>
2026-04-22 10:49:42 +0300

    feat: Wrap Layout component with ProjectsProvider in AdminLayout

--- apps/web/src/app/admin/layout.tsx
+++ apps/web/src/app/admin/layout.tsx
@@ -1,9 +1,10 @@
 import Layout from "@/components/admin/Layout";
+import { ProjectsProvider } from "@/providers/ProjectsContext";
 
 export interface AdminLayoutProps {
   children: React.ReactNode;
 }
 
 export default async function AdminLayout({ children }: AdminLayoutProps) {
-  return await Layout({ children });
+  return <ProjectsProvider>{await Layout({ children })}</ProjectsProvider>;
 }
